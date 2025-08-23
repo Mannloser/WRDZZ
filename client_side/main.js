6d90e8a01933cdee5ed4209b3ed8778a70e0b2f0
Mannloser <patelman542@gmail.com>
2025-08-23 17:40:43 +0530

    23Aug
    index> top selling
    other pages> navbar and footer

--- client_side/main.js
+++ client_side/main.js
@@ -3,3 +3,32 @@ window.addEventListener("load", () => {
     document.getElementById("loader").style.display = "none";
     }, 1950); 
 });
+
+
+//nav bar scroll effect
+document.addEventListener("DOMContentLoaded", () => {
+
+  const navbar = document.querySelector(".navbar");
+  if (!navbar) {
+    console.error("Navbar not found ❌");
+    return;
+  }
+
+  let prevScroll = window.scrollY;
+  const navHeight = navbar.offsetHeight;
+
+  window.addEventListener("scroll", () => {
+
+    let currentScroll = window.scrollY;
+
+    if (currentScroll > prevScroll) {
+      // scrolling down → hide
+      navbar.style.top = `-${navHeight}px`;
+    } else {
+      // scrolling up → show
+      navbar.style.top = "0";
+    }
+
+    prevScroll = currentScroll;
+  });
+});
